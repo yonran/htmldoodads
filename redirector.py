9477dcbd7d8ed12d66b76477dec5346dc6be171c
Yonathan <yonathan@gmail.com>
2011-05-22 00:28:48 -0700

    Now I redirect htmldoodads.appspot.com -> www.rulernow.com to make use of the domain I bought.

--- redirector.py
+++ redirector.py
@@ -1,10 +1,32 @@
 from google.appengine.ext import webapp
 import wsgiref.handlers
+import datetime, time
+import logging, os
 class RedirectHandler(webapp.RequestHandler):
+  ruler_data = open(os.path.join(os.path.dirname(__file__), 'ruler.html')).read()
   def get(self):
-    self.redirect("/ruler.html")
+    logging.info(self.request.path)
+    if 'www.rulernow.com' != self.request.host or '/ruler.html' != self.request.path:
+      if self.request.scheme == 'https':
+        scheme = 'https'
+      else:
+        scheme = 'http'
+      self.redirect(scheme + "://www.rulernow.com/ruler.html")
+    else:
+      self.response.headers['Content-type'] = 'text/html'
+      self.response.headers['Expires'] = wsgiref.handlers.format_date_time(
+          time.mktime((datetime.datetime.now() + datetime.timedelta(days=7)).
+          timetuple()))
+      self.response.headers['Cache-Control'] = 'public, max-age=%d'%(60*60*24*7)
+      self.response.out.write(self.ruler_data)
+
 def main():
-  application = webapp.WSGIApplication([(r'/', RedirectHandler)], debug=True)
+  application = webapp.WSGIApplication(
+    [
+      (r'/', RedirectHandler),
+      (r'/ruler.html', RedirectHandler),
+    ],
+    debug=True)
   wsgiref.handlers.CGIHandler().run(application)
 
 if __name__ == '__main__':
